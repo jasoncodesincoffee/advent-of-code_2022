4bea226c816d3fdf5f568e541ce998d4a5156593
Jason Moseley <jason.moseley@meraki.net>
2022-12-06 09:26:11 -0800

    day6 commit refactor

--- day6.py
+++ day6.py
@@ -3,22 +3,16 @@ line = input.readline()
 packet_marker_length = 4
 message_marker_length = 14
 
-for i in range(len(line)):
-    marker = line[i:i+packet_marker_length]
-    marker_list = []
-    marker_list[:0] = marker
-    dupe_search = set(marker_list)
-    if len(dupe_search) == packet_marker_length:
-        print(f'The number of characters that need to be processed is {i+packet_marker_length}.')
-        break
+def code_breaker(string, marker_length):
+    for i in range(len(string)):
+        marker = string[i:i + marker_length]
+        marker_list = []
+        marker_list[:0] = marker
+        dupe_search = set(marker_list)
+        if len(dupe_search) == marker_length:
+            print(f'The number of characters that need to be processed is {i + marker_length}.')
+            break
 
-############################################
 
-for i in range(len(line)):
-    marker = line[i:i+message_marker_length]
-    marker_list = []
-    marker_list[:0] = marker
-    dupe_search = set(marker_list)
-    if len(dupe_search) == message_marker_length:
-        print(f'The number of characters that need to be processed is {i+message_marker_length}.')
-        break
+code_breaker(line, packet_marker_length)
+code_breaker(line, message_marker_length)
